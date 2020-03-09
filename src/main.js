25cfbfc7dcaa3212fc706e9f112139bb5dd6da1e
greciagarciav <garciav.grecia@gmail.com>
2020-03-09 17:25:18 -0500

    función que trae la data de la HU2

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
-import { example } from './data.js';
+// import { example } from './data.js';
 // import data from './data/injuries/injuries.js';
 // import data from './data/lol/lol.js';
 // import data from './data/patient/patient.js';
+// eslint-disable-next-line import/extensions
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 // import data from './data/steam/steam.js';
 // import data from './data/worldbank/worldbank.js';
 
-console.log(example, data);
+const btnStart = document.getElementById('start');
+
+// eslint-disable-next-line no-shadow
+const allPokemons = (data) => {
+  let mainView = '';
+  data.pokemon.forEach((obj) => {
+    mainView += `
+        <img class="img1" src="${obj.img}"/>
+        <p>${obj.num}</p> 
+        <p>${obj.name}</p>
+        <p>${obj.type}</p>`;
+  });
+  return mainView;
+};
+
+btnStart.addEventListener('click', () => {
+  document.getElementById('root').innerHTML = allPokemons(data);
+});
