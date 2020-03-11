d153383d151110238b9f841e1697c97c6657ebee
ElinAlice <nelializ02@gmail.com>
2020-03-11 17:37:46 -0500

    Implementacion de menu y carga de imagenes de type Pokemon

--- src/main.js
+++ src/main.js
@@ -14,11 +14,17 @@ const btnStart = document.getElementById('start');
 const allPokemons = (data) => {
   let mainView = '';
   data.pokemon.forEach((obj) => {
+    let imgType = '';
+    obj.type.forEach((typeActual) => {
+      imgType += `<img src="./img/types-pokemon/${typeActual}.png"/>`;
+    });
+
     mainView += `
         <div class="pokemon">
         <div class="containerImg"><img class="img1" src="${obj.img}"/></div>
         <p>${obj.num}</p> 
         <p>${obj.name}</p>
+        <div>${imgType}</div>
         <p>${obj.type}</p>
         </div>`;
   });
@@ -27,4 +33,35 @@ const allPokemons = (data) => {
 
 btnStart.addEventListener('click', () => {
   document.getElementById('root').innerHTML = allPokemons(data);
+
+  // const imgClick = document.getElementsByClassName('img1');
+  // for(let i = 0; i < imgClick.length; i++)
+  // {
+  // }
+  // imgClick.forEach((clickMostrar) => {
+  //   clickMostrar.addEventListener('click', () => {
+  //     alert('aqui esta');
+  //   });
+  // });
 });
+
+const btnModal = document.getElementById('btnModal');
+const close = document.getElementById('close');
+
+
+btnModal.addEventListener('click', () => {
+  const containerModal = document.getElementById('containerModal');
+  containerModal.style.display = 'block';
+});
+close.addEventListener('click', () => {
+  const containerModal = document.getElementById('containerModal');
+  containerModal.style.display = 'block';
+});
+
+ 
+// const toogleMenu = document.getElementById('toogleMenu');
+// toogleMenu.addEventListener('click', () => {
+//   const ulMenu = document.getElementById('ulMenu');
+//   let menuToogle = document.getElementById('menuToogle');
+//   menuToogle = ulMenu.nextSibling;
+// });
