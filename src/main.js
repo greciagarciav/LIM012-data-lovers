19077d913468f10b980ce41366e022cde0f9b0a5
greciagarciav <garciav.grecia@gmail.com>
2020-03-20 18:20:24 -0500

    estilos responsive sticky menu

--- src/main.js
+++ src/main.js
@@ -6,18 +6,38 @@ import { orderedMa } from './data.js';
 import { orderedMe } from './data.js';
 import data from './data/pokemon/pokemon.js';
 
+// Sticky Menu Navbar
 const navbar = document.getElementById('navbar');
+const searchContainer = document.getElementById('search');
+const resultSearch = document.getElementById('ulSearch');
+const mediaQueriesMin = window.matchMedia('(min-width:360px)');
+const mediaQueriesMax = window.matchMedia('(max-width:760px)');
 const sticky = navbar.offsetTop;
 
 const stickyNavbar = () => {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add('sticky');
+    resultSearch.classList.add('sticky');
+    resultSearch.style.position = 'fixed';
+    resultSearch.style.top = '65px';
+    searchContainer.classList.add('sticky');
+    searchContainer.style.position = 'relative';
   } else {
     navbar.classList.remove('sticky');
+    resultSearch.classList.remove('sticky');
+    resultSearch.style.position = 'absolute';
+    resultSearch.style.top = '';
+    searchContainer.classList.remove('sticky');
+    searchContainer.style.position = '';
+  }
+  if (mediaQueriesMin.matches && mediaQueriesMax.matches && window.pageYOffset >= sticky) {
+    resultSearch.style.top = '42px';
+  } else {
+    resultSearch.style.position = 'fixed';
   }
 };
 
-window.onscroll = () => { stickyNavbar() };
+window.onscroll = () => stickyNavbar();
 
 const btnStart = document.getElementById('start');
 const searchPokemon = document.getElementById('searchPokemon');
